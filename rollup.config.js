3969d80c586c8e77b2dea8e1154f55d9fc918af3
naihe138 <370215230@qq.com>
2021-05-24 10:01:20 +0800

    init

---
+++ rollup.config.js
@@ -0,0 +1,46 @@
+import vuePlugin from 'rollup-plugin-vue'
+import resolve from '@rollup/plugin-node-resolve';
+import alias from '@rollup/plugin-alias';
+import PostCSS from 'rollup-plugin-postcss'
+import livereload from 'rollup-plugin-livereload'
+import serve from 'rollup-plugin-serve'
+const extensions = ['.js', '.ts', '.vue']
+
+const plugins = [
+  alias({
+    entries: {
+      'vue': 'vue/dist/vue.runtime.esm-browser.js',
+    }
+  }),
+  resolve({ extensions, browser: true }),
+  vuePlugin({
+    template: {
+      compiler: require('@vue/compiler-sfc')
+    },
+    cssModulesOptions: {
+      generateScopedName: '[local]___[hash:base64:5]',
+    },
+  }),
+  PostCSS(),
+]
+
+if (process.env.NODE_ENV === 'development') {
+  plugins.push(livereload())
+  plugins.push(serve({
+    port: 10001,
+    historyApiFallback: true,
+    contentBase: ['./', 'dist', 'node_modules']
+  }))
+}
+
+export default {
+  input: './src/index.js',
+  external: [],
+  plugins,
+  output: [
+    {
+      file: 'dist/bundle.js',
+      format: 'esm',
+    },
+  ],
+}
---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <h1>
+    Hello <span class="aaa">{{ name }}</span>
+  </h1>
+</template>
+
+<script>
+import { defineComponent } from '../vue';
+export default defineComponent({
+  props: {
+    name: {
+      type: String,
+      default: "world"
+    }
+  }
+})
+</script>
+
+<style scoped>
+.aaa {
+  color: blue;
+}
+</style>
